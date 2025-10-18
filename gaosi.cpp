dba5adba15c6b039518b01bc336de728c717df7d
TOWN1949 <toyohoon@outlook.com>
2025-10-18 23:36:28 +0800

    2025年10月18日

---
+++ gaosi.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=0;
+    int sum=0;
+    while(i<=n)
+    {
+        sum+=i;
+       
+        i++;
+    }
+
+    cout<<sum;
+    return 0;
+}
