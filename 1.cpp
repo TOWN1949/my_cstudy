e2b894e7209ae075deecda020d9feefbd0bf84d2
TOWN1949 <toyohoon@outlook.com>
2025-12-01 16:31:20 +0800

    2025年12月1日16:31:17

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c=255;
+    int i=255;
+    cout<<c<<endl;
+    cout<<i<<endl;
+
+
+    return 0;
+}
---
+++ jiadaoyibaoi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    int total=0;
+    for(int i=0;i<=n;i++)
+    {total+=i;
+
+    }
+cout<<total;
+
+
+    return 0;
+}
